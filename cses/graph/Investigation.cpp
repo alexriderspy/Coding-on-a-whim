c831d280785d075eeec0229c3b522cda5c504dd1
alexriderspy <sreemantidey1234@gmail.com>
2022-05-11 16:08:45 +0530

    2 probs

--- cses/graph/Investigation.cpp
+++ cses/graph/Investigation.cpp
@@ -1,5 +1,5 @@
 //k dijkstra - compare 
-
+ 
 #include<iostream>
 #include <queue>
 #include <vector>
@@ -7,15 +7,15 @@
 #include <climits>
 #include <map>
 #include <numeric>
-
+ 
 #define pii pair<int,int>
-
+ 
 using namespace std;
-
+ 
 #define int long long
-
+ 
 const int oo = 1LL<<62;
-
+const int mod = 1e9+7;
 signed main(){
     int n,m;cin>>n>>m;
     vector<pii>edges[n];
@@ -27,7 +27,34 @@ signed main(){
     }
     
     priority_queue<pii>pq;
-
+    vector<int>dist(n,oo),num(n),maxf(n),minf(n,oo);
+    dist[0]=0;
+    minf[0]=0;
+    num[0]=1;
     pq.push({0,0});
-
+ 
+    while(!pq.empty()){
+        pii curr = pq.top();
+        int u = curr.second;
+        pq.pop();
+        if(dist[u]!=-curr.first) continue;
+        for(pii vk:edges[u]){
+            int v = vk.first;
+            int wt = vk.second;
+            if(dist[u] + wt < dist[v]){
+                dist[v] = dist[u] + wt;
+                maxf[v] = maxf[u] + 1;
+                minf[v] = minf[u] + 1;
+                num[v] = num[u];
+                pq.push({-dist[v],v});
+            }else if(dist[u] + wt == dist[v]){
+                maxf[v] = max(maxf[v],maxf[u]+1);
+                minf[v] = min(minf[v],minf[u]+1);
+                num[v] = (num[u] + num[v])%mod;
+            }
+ 
+        }
+    }
+    cout<<dist[n-1] << ' '<<num[n-1]<<' '<<minf[n-1] <<' '<<maxf[n-1]<<'\n';
+ 
 }
---
+++ cses/graph/Planet Queries i.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+    int n,q;cin>>n>>q;
+    vector<int>t(n);
+    for(int i=0;i<n;++i) cin>>t[i];
+    while(q--){
+        int x,k;cin>>x>>k;
+        
+    }
+}
